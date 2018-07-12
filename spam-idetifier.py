69199b4214a118a21788338f072eb63f769f15a3
Vishal Ranjan <vishivish18@gmail.com>
2018-07-13 00:54:39 +0530

    spam filter update

--- spam-idetifier.py
+++ spam-idetifier.py
@@ -50,3 +50,57 @@ messages.head()
 
 messages.describe()
 
+
+# In[16]:
+
+
+messages['length'] = messages['message'].apply(len)
+
+
+# In[19]:
+
+
+messages.head()
+
+
+# In[20]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[21]:
+
+
+import seaborn as sns
+
+
+# In[22]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[30]:
+
+
+messages['length'].plot.hist(bins=550)
+
+
+# In[31]:
+
+
+messages['length'].describe()
+
+
+# In[33]:
+
+
+messages[messages['length']==910]
+
+
+# In[34]:
+
+
+messages.hist(column='length',by='label',bins=60,figsize=(12,4))
+
